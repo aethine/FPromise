1593d4b612871445eee68a64e22d5a53726e9d74
Aethine <yepicness@gmail.com>
2018-09-30 15:59:17 -0400

    First implementation of Promiser

--- FSPromise/Promiser.fs
+++ FSPromise/Promiser.fs
@@ -1,4 +1,25 @@
 ﻿namespace FSPromise
 
-type Promiser<'T>() =
-    class end
+module Promiser =
+    open Promises
+    
+
+    type Promiser<'T>() =
+        let mutable hasFinished : Result<'T, exn> option = None
+
+        member _this.Then fn =
+            promise (fun resolve reject ->
+                let rec check () =
+                    match hasFinished with
+                    | None -> check () 
+                    | Some result ->
+                        fn result
+                        match result with
+                        | Ok ok -> resolve ok
+                        | Error e -> reject e
+                check ()
+            )
+        member _this.Resolve v =
+            hasFinished <- Some (Ok v)
+        member _this.Reject e =
+            hasFinished <- Some (Error e)
